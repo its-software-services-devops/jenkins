68e644be800d24834df0a76dabd388e401593a58
Phanuwat.P <phanuwat.prad@gmail.com>
2024-09-25 17:31:54 +0700

    Remove config groovy (test-4)

--- jobs-1.groovy
+++ jobs-1.groovy
@@ -2,13 +2,3 @@ folder('GTT')
 
 folder('GTT/DevOps-Build')
 
-multibranchPipelineJob("GTT/DevOps-Build/astro-devops-training") {
-    branchSources {
-        git {
-            id("astro-devops-training")
-            remote("git@bitbucket.org:astrosolutions-co-th/astro-devops-training.git")
-            credentialsId("PJames-Bitbucket-SSH-Key")
-            includes("develop main")
-        }
-    }
-}
