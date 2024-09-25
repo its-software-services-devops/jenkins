c7903b1a12b748933e136bfd90d51523a7302ab0
Phanuwat.P <phanuwat.prad@gmail.com>
2024-09-25 17:24:38 +0700

    Edited file groovy (test-3)

--- jobs-1.groovy
+++ jobs-1.groovy
@@ -11,43 +11,4 @@ multibranchPipelineJob("GTT/DevOps-Build/astro-devops-training") {
             includes("develop main")
         }
     }
-
-    factory {
-        remoteJenkinsFileWorkflowBranchProjectFactory {
-            remoteJenkinsFile("Jenkinsfile_Common")
-            localMarker("")
-            matchBranches(false)
-            fallbackBranch("master")
-        }
-    }
-
-    configure { project ->
-        def factoryNode = project.factory
-        factoryNode << new org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject(owner: project)
-
-        factoryNode << new hudson.plugins.git.GitSCM {
-            userRemoteConfigs {
-                new hudson.plugins.git.UserRemoteConfig {
-                    url('git@bitbucket.org:astrosolutions-co-th/astro-gtt-jenkins-files.git')
-                    credentialsId('PJames-Bitbucket-SSH-Key')
-                }
-            }
-
-            branches {
-                new hudson.plugins.git.BranchSpec {
-                    name('main')
-                }
-            }                    
-        }
-    }
-
-    triggers {
-        cron('*/5 * * * *')
-    }
-
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
 }
