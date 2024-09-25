3d94f66e01496416169a98b0ac865e05f9486a74
Phanuwat.P <phanuwat.prad@gmail.com>
2024-09-25 17:48:29 +0700

    Config multibranch jobs (test-6)

--- jobs-1.groovy
+++ jobs-1.groovy
@@ -2,18 +2,37 @@ folder('GTT')
 
 folder('GTT/DevOps-Build')
 
-pipelineJob('job-dsl-plugin') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                remote {
-                    url('https://github.com/jenkinsci/job-dsl-plugin.git')
-                }
-                branch('*/master')
+multibranchPipelineJob('astro-devops-training) {
+    branchSources {
+      branchSource {
+        source {
+        git {
+            id('astro-devops-training')
+            remote('git@bitbucket.org:astrosolutions-co-th/astro-devops-training.git')
+                credentialsId('PJames-Bitbucket-SSH-Key')
+        }
+    }
+        strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                    }
                 }
             }
-        lightweight()
+        }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('Jenkinsfile')
+            }
+        }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
         }
     }
 }
