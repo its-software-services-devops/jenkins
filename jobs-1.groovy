78a1f9ff82f5fd639bcb790b45a81332194c7469
Phanuwat.P <phanuwat.prad@gmail.com>
2024-09-25 17:50:53 +0700

    Config multibranch jobs (test-7)

--- jobs-1.groovy
+++ jobs-1.groovy
@@ -2,37 +2,55 @@ folder('GTT')
 
 folder('GTT/DevOps-Build')
 
-multibranchPipelineJob('astro-devops-training) {
-    branchSources {
-      branchSource {
-        source {
-        git {
-            id('astro-devops-training')
-            remote('git@bitbucket.org:astrosolutions-co-th/astro-devops-training.git')
-                credentialsId('PJames-Bitbucket-SSH-Key')
-        }
+multibranchPipelineJob("astro-devops-training")
+{
+  branchSources {
+    git {
+      id = "premium-service-v2"
+      remote("git@bitbucket.org:astrosolutions-co-th/astro-devops-training.git")
+      credentialsId("PJames-Bitbucket-SSH-Key")
+      includes("development sit uat production")
     }
-        strategy {
-                    defaultBranchPropertyStrategy {
-                        props {
-                            noTriggerBranchProperty()
-                    }
-                }
-            }
-        }
-    }
-    configure {
-        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
-    }
-        factory {
-            workflowBranchProjectFactory {
-                scriptPath('Jenkinsfile')
-            }
-        }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
-    }
+  }
+  factory
+  {
+      remoteJenkinsFileWorkflowBranchProjectFactory 
+      {
+          remoteJenkinsFile("Jenkinsfile_Common")
+          localMarker("")
+          matchBranches(false)
+          fallbackBranch("master")
+      }
+  }
+  configure { project ->
+      def factoryNode = project / 'factory'
+      factoryNode << owner(class:'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference:'../..')
+  
+      factoryNode << remoteJenkinsFileSCM(class:'hudson.plugins.git.GitSCM') {
+          userRemoteConfigs 
+          {
+              'hudson.plugins.git.UserRemoteConfig' 
+              {
+                  url('git@bitbucket.org:astrosolutions-co-th/astro-gtt-jenkins-files.git')
+                  credentialsId('PJames-Bitbucket-SSH-Key')
+              }
+          }
+          branches 
+          {
+              'hudson.plugins.git.BranchSpec' 
+              {
+                  name('main')
+              }
+          }                    
+      }
+  }
+  triggers
+  {
+      cron('*/5 * * * *')
+  }
+  orphanedItemStrategy {
+      discardOldItems {
+          numToKeep(20)
+      }
+  }
 }
