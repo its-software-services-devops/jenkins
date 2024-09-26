5c2b5b35c4dbfa91ad6bb4b8f7f06796e445a726
Phanuwat.P <phanuwat.prad@gmail.com>
2024-09-26 17:43:58 +0700

    Test config groovy file

--- jobs-1.groovy
+++ jobs-1.groovy
@@ -1,5 +1,45 @@
-folder('GTT')
-
-folder('GTT/DevOps-Build')
-
-folder('GTT/Build')
+multibranchPipelineJob("astro-devops-training") 
+    {
+    branchSources {
+        git {
+            id = "astro-devops-training"
+            remote("git@bitbucket.org:astrosolutions-co-th/astro-devops-training.git")
+            credentialsId("PJames-Bitbucket-SSH-Key")
+            includes("develop main")
+        }
+    }
+    factory{
+        remoteJenkinsFileWorkflowBranchProjectFactory{
+        localMarker('')
+        remoteJenkinsFile("Jenkinsfile_Common")
+        remoteJenkinsFileSCM{
+            gitSCM{
+            userRemoteConfigs{
+                userRemoteConfig{
+                name("origin")
+                url("git@bitbucket.org:astrosolutions-co-th/astro-gtt-jenkins-files.git")
+                refspec("+refs/heads/main:refs/remotes/origin/main")
+                credentialsId("PJames-Bitbucket-SSH-Key")
+                }
+            }
+            branches {
+                branchSpec {
+                    name("main")
+                }
+            }
+            browser{}
+            gitTool("")
+            }
+        }
+        }
+    }
+    triggers
+    {
+        cron('*/5 * * * *')
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
